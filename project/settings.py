45d043c90d56dc7597107b2d0fec7356f5b6774a
vikalp <kumarvikalp48@gmail.com>
2025-07-05 02:10:10 +0530

    changes for staticfiles

--- project/settings.py
+++ project/settings.py
@@ -153,7 +153,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
