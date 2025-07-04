b7b64b4b34e0088f1c0516a783feb472ca5c59e4
vikalp <kumarvikalp48@gmail.com>
2025-07-05 01:49:54 +0530

    Update build script to install Python and dependencies
    The build_files.sh script now installs sqlite-devel, configures and builds Python with optimizations and loadable SQLite extensions, and ensures the environment is set up before installing requirements and collecting static files.

--- project/build_files.sh
+++ project/build_files.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+# build_files.sh
+which python3.11
+pwd
+cd /usr/local/bin/
+yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make && make altinstall
+cd /vercel/path0/
 pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput
