10c3c3ac21c5a1fb16d15da843135350fc577a4c
vikalp <kumarvikalp48@gmail.com>
2025-07-05 03:20:32 +0530

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-# build_files.sh
-
-#!/bin/bash
-
-# Use the default Python and pip available in Vercel's build environment
-pip install -r requirements.txt
-
-# Run collectstatic to gather static files
-python manage.py collectstatic --noinput
